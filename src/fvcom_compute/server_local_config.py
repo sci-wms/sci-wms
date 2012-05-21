d2eeeec8a5d27dbc997a080c9446405f79be397a
Alexander Crosby <acrosby@asascience.com>
2012-05-21 11:24:36 -0400

    acrosby_cleanup_changes

--- src/fvcom_compute/server_local_config.py
+++ src/fvcom_compute/server_local_config.py
@@ -4,7 +4,7 @@
 #
 
 # local path to database (doesn't include file name)
-dbpath = "/home/acrosby/SCI056OPT780/Documents and Settings/ACrosby/My Documents/Eclipse/fvcom_compute/src/"
+dbpath = "/home/acrosby/Development/Python/unstructured_server/fvcom_wms/src/"
 
 # local path to statics (doesn't include file name)
 staticspath = "/home/acrosby/SCI056OPT780/Documents and Settings/ACrosby/My Documents/Eclipse/fvcom_compute/src/fvcom_compute/fvcom_stovepipe/static/"
@@ -16,7 +16,7 @@ datasetpath = "http://www.smast.umassd.edu:8080/thredds/dodsC/fvcom/hindcasts/30
 
 # if this is populated, the service will use the dataset above for schema/grid information
 # and the dataset below for actual data extraction.
-localdataset = True
+localdataset = False
 #localpath = "gom3_197802.nc"
 localpath = "/home/acrosby/Development/Data/FVCOM/gom3_197802.nc"
 
