49b5f66f717127e3c072d59fd8b77a9649b40bba
Alexander Crosby <acrosby@asascience.com>
2012-06-21 16:41:10 -0400

    updates to gunicorn config file to check for different async workers and then default to sync if not found

--- src/pywms/config.py
+++ src/pywms/config.py
@@ -1,11 +1,22 @@
 import multiprocessing
-
+try:
+    import gevent
+    worker = "gevent"
+except:
+    try:
+        import eventlet 
+        worker = "eventlet"
+    except:
+        # Default to basic sync worker if other libs are
+        # not installed
+        worker = "sync"
+    
 bind = "0.0.0.0:7000"
 workers = 12#multiprocessing.cpu_count()
-worker_class = "gevent"
+worker_class = worker
 debug = True
 timeout = 1000
 max_requests = 16
 backlog = 100
 
-#name = "unstructuredserver"
+name = "pywms"
