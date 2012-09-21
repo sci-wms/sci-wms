eff931776ea3e33d6f17e099585822f2391c4ea6
Alexander Crosby <acrosby@asascience.com>
2012-09-21 10:08:46 -0400

    removing log classes

--- src/pywms/log/null_handler.py
+++
@@ -1,9 +0,0 @@
-try:
-    # Python 2.7
-    from logging import NullHandler
-except ImportError:
-    # Python < 2.7
-    import logging
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
