9353123f6bf00ab9e01e51ee69b0d2569ab96ea6
Alexander Crosby <acrosby@asascience.com>
2013-01-30 12:39:21 -0500

    some changes for edges of cgrid images

--- src/pywms/wms/cgrid.py
+++ src/pywms/wms/cgrid.py
@@ -9,8 +9,8 @@ def subset(latmin, lonmin, latmax, lonmax, lat, lon):
     index = np.asarray(np.where(latbool & lonbool)).squeeze()
         #((lat <= latmax) == (lat >= latmin)) ==
         #((lon <= lonmax) == (lon >= lonmin),))).squeeze()
-    lon[lon > lonmax] = np.nan # would prefer to be subsetting the smallest area possible isntead of just hacking the rendering...
-    lon[lon < lonmin] = np.nan
+    lon[lon > lonmax+.18] = np.nan # would prefer to be subsetting the smallest area possible isntead of just hacking the rendering...
+    lon[lon < lonmin-.18] = np.nan
     if index.shape[1] > 0:
         ind = np.asarray(range(np.min(np.min(index[0])),np.max(np.max(index[0]))+1))
         jnd = np.asarray(range(np.min(np.min(index[1])),np.max(np.max(index[1]))+1))
