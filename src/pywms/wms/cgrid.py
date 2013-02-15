fix for multipolygon masking problem in ugrid.py

src/pywms/wms/cgrid.py
@@ -2,6 +2,7 @@
 import os, sys
 import numpy as np
 from matplotlib.pylab import get_cmap
+from matplotlib.mlab import griddata
 
 def subset(latmin, lonmin, latmax, lonmax, lat, lon):
     latbool = (lat <= latmax+.18) & (lat >= latmin-.18)
@@ -117,10 +118,15 @@ def plot(lon, lat, var1, var2, actions, ax, fig, **kwargs):
             fig.set_figheight(height/80.0/aspect)
             fig.set_figwidth(width/80.0)
             unit_vectors(lon, lat, var1, var2, mag, ax, norm, cmap, magnitude)
-        #elif "streamlines" in actions:
-        #    fig.set_figheight(height/80.0/aspect)
-        #    fig.set_figwidth(width/80.0)
-        #    streamlines(lon, lat, var1, var2, mag, ax, norm, cmap, magnitude)
+        elif "streamlines" in actions:
+            fig.set_figheight(height/80.0/aspect)
+            fig.set_figwidth(width/80.0)
+            m = kwargs.get('basemap')
+            lonmin = kwargs.get("lonmin")
+            latmin = kwargs.get("latmin")
+            lonmax = kwargs.get("lonmax")
+            latmax = kwargs.get("latmax")
+            streamlines(lon, lat, var1, var2, mag, ax, norm, cmap, magnitude, m, lonmin, latmin, lonmax, latmax)
         elif "barbs" in actions:
             fig.set_figheight(height/80.0/aspect)
             fig.set_figwidth(width/80.0)
@@ -200,7 +206,7 @@ def unit_vectors(lon, lat, var1, var2, mag, ax, norm, cmap, magnitude):
                 angles='uv',
                 )
                 
-def streamlines(lon, lat, var1, var2, mag, ax, norm, cmap, magnitude):
+def streamlines(lon, lat, var1, var2, mag, ax, norm, cmap, magnitude, m, lonmin, latmin, lonmax, latmax):
     if magnitude == "True":
         arrowsize = None
     elif magnitude == "False":
@@ -210,12 +216,32 @@ def streamlines(lon, lat, var1, var2, mag, ax, norm, cmap, magnitude):
     else:
         arrowsize = float(magnitude)
     stride = 1
-    ax.streamplot(lon[::stride,::stride], lat[::stride,::stride], var1.squeeze()[::stride,::stride], var2.squeeze()[::stride,::stride], color=mag.squeeze()[::stride,::stride],
-                density=6,
-                linewidth=5*mag.squeeze()[::stride,::stride]/mag.squeeze()[::stride,::stride].max(),
-                cmap=cmap,
-                norm=norm,
-                )
+    try:
+        ax.streamplot(lon[::stride,::stride], lat[::stride,::stride], 
+                      var1.squeeze()[::stride,::stride], var2.squeeze()[::stride,::stride], 
+                      color=mag.squeeze()[::stride,::stride],
+                      density=6,
+                      linewidth=5*mag.squeeze()[::stride,::stride]/mag.squeeze()[::stride,::stride].max(),
+                      cmap=cmap,
+                      norm=norm,
+                      )
+    except:
+        num = int( (lonmax - lonmin) * 50000)#320 )
+        xi = np.arange(m.xmin, m.xmax, num)
+        yi = np.arange(m.ymin, m.ymax, num)
+        lat, lon, mag, var1, var2 = lat.astype(np.float64).flatten(), lon.astype(np.float64).flatten(), mag.astype(np.float64).flatten(), var1.astype(np.float64).flatten(), var2.astype(np.float64).flatten()
+        print lat.shape, mag.shape, var1.shape
+        mag = griddata(lon, lat, mag, xi, yi, interp='nn')
+        var1 = griddata(lon, lat, var1, xi, yi, interp='nn')
+        var2 = griddata(lon, lat, var2, xi, yi, interp='nn')
+        ax.streamplot(xi, yi, 
+                      var1, var2, 
+                      color=mag,
+                      density=6,
+                      linewidth=5*mag/mag.max(),
+                      cmap=cmap,
+                      norm=norm,
+                      )
 
 def barbs(lon, lat, var1, var2, mag, ax, norm, cmin, cmax, cmap, magnitude):
     if magnitude == "True":

src/pywms/wms/ugrid.py
@@ -134,6 +134,9 @@ def pcolor(lon, lat, lonn, latn, mag, nv, m, ax, norm, cmap, topology_type, fig,
         zi = griddata(lon, lat, mag, xi, yi, interp='nn')
     fig, m, patch1 = cookie_cutter(dataset, fig, m, lonmin, latmin, lonmax, latmax, projection, continuous)
     m.imshow(zi, norm=norm, cmap=cmap, clip_path=patch1, interpolation="nearest")
+    #from matplotlib.backends.backend_agg import FigureCanvasAgg
+    #canvas = FigureCanvasAgg(fig)
+    #canvas.print_png("testing_yay.png")
     return fig, m
 
 def contour(lon, lat, lonn, latn, mag, nv, m, ax, norm, cmin, cmax, cmap, topology_type, fig, height, width, lonmin, latmin, lonmax, latmax, dataset, continuous, projection):
@@ -390,6 +393,7 @@ def get_domain_as_patch(dataset, m, lonmin, latmin, lonmax, latmax, continuous):
     domain = domain.intersection(box)
     # Create a path out of the polygon for clipping
     if domain.geom_type == "Polygon":
+        print "poly"
         x, y = domain.exterior.xy
         x = np.asarray(x)
         # Correct continous
@@ -409,15 +413,21 @@ def get_domain_as_patch(dataset, m, lonmin, latmin, lonmax, latmax, continuous):
             y = np.concatenate((y, holey))
         p = mpath.Path(np.asarray((x,y)).T, codes = allcodes)
     elif domain.geom_type == "MultiPolygon":
+        print "multi"
         for i, part in enumerate(domain.geoms):
-            x, y = part.exterior.xy
-            x = np.asarray(x)
+            x1, y1 = part.exterior.xy
+            x1 = np.asarray(x1)
             # Correct continous
-            x = correct_continuous(x, continuous, lonmin, latmin, lonmax, latmax)
-            x, y = m(x, y)
-            x = np.hstack((np.asarray(x),x[0]))
-            y = np.hstack((np.asarray(y),y[0]))
-            allcodes = create_path_codes(x)
+            x1 = correct_continuous(x1, continuous, lonmin, latmin, lonmax, latmax)
+            x1, y1 = m(x1, y1)
+            if i != 0:
+                allcodes = add_path_codes(x1, allcodes)
+                x = np.concatenate((x, x1))
+                y = np.concatenate((y, y1))
+            else:
+                x = np.hstack((np.asarray(x1),x1[0]))
+                y = np.hstack((np.asarray(y1),y1[0]))
+                allcodes = create_path_codes(x)
             try:
                 for hole in part.interiors:
                     holex, holey = hole.xy