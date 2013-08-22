21d5e5ec070d02c5c346f2342f70f6bf39892829
Alexander Crosby <acrosby@asascience.com>
2013-08-22 14:36:02 -0400

    removing extraneous bool statement in cgrid.py

--- src/pywms/wms/cgrid.py
+++ src/pywms/wms/cgrid.py
@@ -128,7 +128,7 @@ def getvar(datasetnc, t, layer, variables, index):
         if var1 != None:
             if "additional_fill_values" in ncvar1.ncattrs():
                 for fillval in map(float, ncvar1.additional_fill_values.split(",")):
-                    temp = var1==fillval
+                    #temp = var1==fillval
                     #var1.mask[var1==fillval] = True
                     var1[var1==fillval] = np.nan
         if var2 != None:
