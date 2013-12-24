6e6bc6930979f646b338a8b172361eb0e647c5a2
Kyle Wilcox <kyle@axiomalaska.com>
2013-12-24 11:47:26 -0500

    Catch topology errors and present a warning

--- sciwms/apps/wms/startup.py
+++ sciwms/apps/wms/startup.py
@@ -1,7 +1,15 @@
 def run():
     print "Initializing datasets topologies..."
     from sciwms.libs.data.grid_init_script import init_all_datasets
-    init_all_datasets()
+    try:
+        init_all_datasets()
+    except BaseException:
+        print '\n    ###################################################\n' +\
+              '    #                                                 #\n' +\
+              '    #  There was a problem initializing some of your  #\n' +\
+              '    #  datasets.  Please see the log for more details #\n' +\
+              '    #                                                 #\n' +\
+              '    ###################################################\n'
 
     print '\n    ##################################################\n' +\
           '    #                                                #\n' +\
