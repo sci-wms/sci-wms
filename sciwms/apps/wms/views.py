2ca9ccdb39080eeec81a4b09d026701f70b4e8ec
Kyle Wilcox <kyle@axiomdatascience.com>
2015-03-25 16:52:05 -0400

    Fix path typo in GetFeatureInfo

--- sciwms/apps/wms/views.py
+++ sciwms/apps/wms/views.py
@@ -979,7 +979,7 @@ def getFeatureInfo(request, dataset):
     if gridtype == u'False':
         test_index = 0
         if 'node' in styles:
-            tree = rindex.Index(dataset.node_tree_index_file)
+            tree = rindex.Index(dataset.node_tree_root)
             #lats = topology.variables['lat'][:]
             #lons = topology.variables['lon'][:]
             nindex = list(tree.nearest((lon, lat, lon, lat), 1, objects=True))
@@ -1008,7 +1008,7 @@ def getFeatureInfo(request, dataset):
         index = nindex[test_index].id
         tree.close()
     else:
-        tree = rindex.Index(dataset.node_tree_index_file)
+        tree = rindex.Index(dataset.node_tree_root)
         lats = topology.variables['lat'][:]
         lons = topology.variables['lon'][:]
         nindex = list(tree.nearest((lon, lat, lon, lat), 1, objects=True))
