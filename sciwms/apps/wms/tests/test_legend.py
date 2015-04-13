4c8bf91438a4df2eae4d2ef668bebb8776f4cfe4
Kyle Wilcox <kyle@axiomdatascience.com>
2015-04-13 11:01:14 -0400

    Add tests for GetLegendGraphic

---
+++ sciwms/apps/wms/tests/test_legend.py
@@ -0,0 +1,71 @@
+from django.test import TestCase
+from sciwms.apps.wms.tests import *
+from sciwms.apps.wms.models import Dataset
+
+
+class TestGetLegendGraphic(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        add_server()
+        add_group()
+        add_user()
+        add_dataset("nasa_scb20111015.nc")
+
+    @classmethod
+    def tearDownClass(cls):
+        d = Dataset.objects.get(name='test')
+        d.clear_cache()
+        d.delete()
+
+    def test_pcolor(self):
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=facets_average_jet_-1_1_cell_False&layer=u%2Cv')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=facets_average_jet_-1_1_cell_False&layer=u%2Cv&showlabel=false')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=facets_average_jet_-1_1_cell_False&layer=u%2Cv&units=somethingelse')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=facets_average_jet_None_None_cell_False&layer=u%2Cv&colorscalerange=-1,1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contours(self):
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=contours_average_jet_-1_1_cell_False&layer=u%2Cv')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=contours_average_jet_-1_1_cell_False&layer=u%2Cv&showlabel=false')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=contours_average_jet_-1_1_cell_False&layer=u%2Cv&units=somethingelse')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=contours_average_jet_None_None_cell_False&layer=u%2Cv&colorscalerange=-1,1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_filledcontours(self):
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=filledcontours_average_jet_-1_1_cell_False&layer=u%2Cv')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=filledcontours_average_jet_-1_1_cell_False&layer=u%2Cv&showlabel=false')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=filledcontours_average_jet_-1_1_cell_False&layer=u%2Cv&units=somethingelse')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=filledcontours_average_jet_None_None_cell_False&layer=u%2Cv&colorscalerange=-1,1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_vectors(self):
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=vectors_average_jet_-1_1_cell_False&layer=u%2Cv')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=vectors_average_jet_-1_1_cell_False&layer=u%2Cv&showlabel=false')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=vectors_average_jet_-1_1_cell_False&layer=u%2Cv&units=somethingelse')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/wms/datasets/test/?request=GetLegendGraphic&styles=vectors_average_jet_None_None_cell_False&layer=u%2Cv&colorscalerange=-1,1')
+        self.assertEqual(response.status_code, 200)
