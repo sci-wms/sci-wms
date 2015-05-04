23cecc53454117ee1a7d385b9f1f1c0a04d13127
Kyle Wilcox <kyle@axiomdatascience.com>
2015-05-04 11:48:34 -0400

    Skip the SGRID tests (for now) and change them to use the new styles syntax

--- sciwms/apps/wms/tests/test_sgrid.py
+++ sciwms/apps/wms/tests/test_sgrid.py
@@ -1,9 +1,12 @@
+import unittest
+
 from django.test import TestCase
 from sciwms.apps.wms.tests import add_server, add_group, add_user, add_dataset
 from sciwms.apps.wms.models import Dataset
 
 
-class TestCgrid(TestCase):
+@unittest.skip("SGRID Datasets are not implemented yet")
+class TestSgrid(TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -18,20 +21,24 @@ class TestCgrid(TestCase):
         d.clear_cache()
         d.delete()
 
+    def test_facets(self):
+        response = self.client.get('/wms/datasets/sgrid_testing/?LAYERS=u%2Cv&TRANSPARENT=TRUE&STYLES=facets_jet&TIME=&ELEVATION=0&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&SRS=EPSG%3A3857&BBOX=-8543030.3273202,5492519.0747705,-8401010.3287862,5542356.0172055&WIDTH=929&HEIGHT=326')
+        self.assertEqual(response.status_code, 200)
+
     def test_pcolor(self):
-        response = self.client.get('/wms/datasets/sgrid_testing/?LAYERS=u%2Cv&TRANSPARENT=TRUE&STYLES=pcolor_average_jet_None_None_cell_False&TIME=&ELEVATION=0&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&SRS=EPSG%3A3857&BBOX=-8543030.3273202,5492519.0747705,-8401010.3287862,5542356.0172055&WIDTH=929&HEIGHT=326')
+        response = self.client.get('/wms/datasets/sgrid_testing/?LAYERS=u%2Cv&TRANSPARENT=TRUE&STYLES=pcolor_jet&TIME=&ELEVATION=0&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&SRS=EPSG%3A3857&BBOX=-8543030.3273202,5492519.0747705,-8401010.3287862,5542356.0172055&WIDTH=929&HEIGHT=326')
         self.assertEqual(response.status_code, 200)
 
     def test_contours(self):
-        response = self.client.get('/wms/datasets/sgrid_testing/?LAYERS=u%2Cv&TRANSPARENT=TRUE&STYLES=contours_average_jet_None_None_cell_False&TIME=&ELEVATION=0&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&SRS=EPSG%3A3857&BBOX=-8543030.3273202,5492519.0747705,-8401010.3287862,5542356.0172055&WIDTH=929&HEIGHT=326')
+        response = self.client.get('/wms/datasets/sgrid_testing/?LAYERS=u%2Cv&TRANSPARENT=TRUE&STYLES=contouts_jet&TIME=&ELEVATION=0&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&SRS=EPSG%3A3857&BBOX=-8543030.3273202,5492519.0747705,-8401010.3287862,5542356.0172055&WIDTH=929&HEIGHT=326')
         self.assertEqual(response.status_code, 200)
 
     def test_filledcontours(self):
-        response = self.client.get('/wms/datasets/sgrid_testing/?LAYERS=u%2Cv&TRANSPARENT=TRUE&STYLES=filledcontours_average_jet_None_None_cell_False&TIME=&ELEVATION=0&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&SRS=EPSG%3A3857&BBOX=-8543030.3273202,5492519.0747705,-8401010.3287862,5542356.0172055&WIDTH=929&HEIGHT=326')
+        response = self.client.get('/wms/datasets/sgrid_testing/?LAYERS=u%2Cv&TRANSPARENT=TRUE&STYLES=filledcontours_jet&TIME=&ELEVATION=0&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&SRS=EPSG%3A3857&BBOX=-8543030.3273202,5492519.0747705,-8401010.3287862,5542356.0172055&WIDTH=929&HEIGHT=326')
         self.assertEqual(response.status_code, 200)
 
     def test_vectors(self):
-        response = self.client.get('/wms/datasets/sgrid_testing/?LAYERS=u%2Cv&TRANSPARENT=TRUE&STYLES=vectors_average_jet_None_None_cell_False&TIME=&ELEVATION=0&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&SRS=EPSG%3A3857&BBOX=-8543030.3273202,5492519.0747705,-8401010.3287862,5542356.0172055&WIDTH=929&HEIGHT=326')
+        response = self.client.get('/wms/datasets/sgrid_testing/?LAYERS=u%2Cv&TRANSPARENT=TRUE&STYLES=vectors_jet&TIME=&ELEVATION=0&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&SRS=EPSG%3A3857&BBOX=-8543030.3273202,5492519.0747705,-8401010.3287862,5542356.0172055&WIDTH=929&HEIGHT=326')
         self.assertEqual(response.status_code, 200)
 
     def test_getCaps(self):
