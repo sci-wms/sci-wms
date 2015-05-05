c3775da81aba6097dfae350cb97f65f79be5064a
Kyle Wilcox <kyle@axiomdatascience.com>
2015-05-05 09:41:35 -0400

    Update datasets in the AppConfig on restart, when not TESTING or DEBUG

--- wms/apps.py
+++ wms/apps.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.apps import AppConfig
+from django.conf import settings
+
+from wms import logger
 
 
 class WmsConfig(AppConfig):
@@ -9,3 +12,16 @@ class WmsConfig(AppConfig):
     def ready(self):
         # Initialize signals
         import wms.signals
+
+        Dataset = self.get_model('Dataset')
+        if settings.TESTING or settings.DEBUG:
+            logger.info("Not updating datasets due to TESTING or DEBUG setting being True")
+        else:
+            for d in Dataset.objects.all():
+                try:
+                    d.update_cache()
+                    logger.info('Updating {} successful'.format(d.name))
+                except NotImplementedError:
+                    logger.info('Updating {} failed.  Dataset type not implemented.'.format(d.name))
+                except BaseException as e:
+                    logger.info('Updating {} failed. {}.'.format(d.name, str(e)))
