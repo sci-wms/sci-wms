ac99a17187c6409cb626a1a72ebd095bcfa88fe1
Kyle Wilcox <kyle@axiomdatascience.com>
2015-11-19 13:41:32 -0500

    Bump to 1.5.0-dev

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -47,9 +47,9 @@ project = u'SCI-WMS'
 # built documents.
 #
 # The short X.Y version.
-version = '1.4.0'
+version = '1.5.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.4.0'
+release = '1.5.0-dev'
 
 rst_epilog = """
 .. |github_repo| replace:: https://github.com/sci-wms/sci-wms
