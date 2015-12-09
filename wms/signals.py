2e7a54bef4ee6b2743c62dd24a7ea488a1a48f24
Kyle Wilcox <kyle@axiomdatascience.com>
2015-12-09 10:51:42 -0500

    Always process_layers when a Dataset is saved. For ugrid_tides signals.

--- wms/signals.py
+++ wms/signals.py
@@ -7,38 +7,30 @@ from wms.models import UGridDataset, SGridDataset, RGridDataset, UGridTideDatase
 
 
 @receiver(post_save, sender=UGridTideDataset)
-def ugrid_dataset_post_save(sender, instance, created, **kwargs):
-    if created is True and settings.TESTING is not True:
-        instance.update_cache()
-        instance.process_layers()
-    elif not instance.has_cache() and settings.TESTING is not True:
+def ugrid_tides_dataset_post_save(sender, instance, created, **kwargs):
+    instance.process_layers()
+    if settings.TESTING is not True and (created is True or not instance.has_cache()):
         instance.update_cache()
 
 
 @receiver(post_save, sender=UGridDataset)
 def ugrid_dataset_post_save(sender, instance, created, **kwargs):
-    if created is True and settings.TESTING is not True:
-        instance.update_cache()
-        instance.process_layers()
-    elif not instance.has_cache() and settings.TESTING is not True:
+    instance.process_layers()
+    if settings.TESTING is not True and (created is True or not instance.has_cache()):
         instance.update_cache()
 
 
 @receiver(post_save, sender=SGridDataset)
 def sgrid_dataset_post_save(sender, instance, created, **kwargs):
-    if created is True and settings.TESTING is not True:
-        instance.update_cache()
-        instance.process_layers()
-    elif not instance.has_cache() and settings.TESTING is not True:
+    instance.process_layers()
+    if settings.TESTING is not True and (created is True or not instance.has_cache()):
         instance.update_cache()
 
 
 @receiver(post_save, sender=RGridDataset)
 def rgrid_dataset_post_save(sender, instance, created, **kwargs):
-    if created is True and settings.TESTING is not True:
-        instance.update_cache()
-        instance.process_layers()
-    elif not instance.has_cache() and settings.TESTING is not True:
+    instance.process_layers()
+    if settings.TESTING is not True and (created is True or not instance.has_cache()):
         instance.update_cache()
 
 
