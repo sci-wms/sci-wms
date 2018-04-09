86fae204ba0b47ef2f00d5f6cf8025a0e0f77b23
Kyle Wilcox <kyle@axiomdatascience.com>
2018-04-09 13:03:34 -0400

    Use the update_dataset task in signal to update the status message

--- wms/signals.py
+++ wms/signals.py
@@ -2,14 +2,13 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete, pre_save
 
-from wms.tasks import process_layers, update_cache, add_unidentified_dataset
+from wms.tasks import update_dataset, add_unidentified_dataset
 from wms.models import UGridDataset, SGridDataset, RGridDataset, UGridTideDataset, UnidentifiedDataset
 
 
 def schedule_dataset_update(sender, instance, created, **kwargs):
     if created is True or not instance.has_cache():
-        process_layers(instance.pk)
-        update_cache(instance.pk)
+        update_dataset(instance.pk)
 
 
 @receiver(post_save, sender=UnidentifiedDataset)
