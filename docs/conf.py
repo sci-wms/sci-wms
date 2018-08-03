54ed8aeae66666a98e680cd3e8ea268a7a3d668b
Kyle Wilcox <kyle@axiomdatascience.com>
2018-08-03 14:22:14 -0400

    Update docs for 2.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ author = 'sci-wms developers'
 # The short X.Y version.
 version = '2.0.0'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.0-dev'
+release = '2.0.0'
 
 rst_epilog = """
 .. |github_repo| replace:: https://github.com/sci-wms/sci-wms
